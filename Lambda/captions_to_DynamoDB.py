4f7861af6137f4fbaa86dd08c35c6069adf55754
Vrajesh Mehta <vmehta342@gmail.com>
2019-05-21 01:39:22 -0400

    scrap the captions from the web and push it to the dynamodb

---
+++ Lambda/captions_to_DynamoDB.py
@@ -0,0 +1,35 @@
+
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    bucketname = "mainfinaldata"
+    itemname = "captions.json"
+    
+    dynamodb = boto3.client('dynamodb')
+    # table = dynamodb.Tablename("captions")
+    
+    s3 = boto3.client('s3',
+                        aws_access_key_id='AKIAYML6HL5K7MKXESCU',
+                        aws_secret_access_key='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')
+    obj = s3.get_object(Bucket = bucketname, Key = itemname)
+    
+    print("here"  + str(obj) + "done")
+    
+    file_content = obj.get('Body').read().decode('utf-8')
+    json_content = json.loads(file_content)
+    
+    print(type(json_content["love"]))
+    for i in json_content:
+        dynamodb.put_item(TableName='captions', Item={ "tag" : {"S" : i}, "quotes" : { "SS" : json_content[i] }})
+
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
